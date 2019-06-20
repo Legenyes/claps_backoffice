382f9fc474fa3b51d9eac698bf2b49111cfb187e
Sébastien Lévêque <sleveque@emisys.eu>
2019-06-20 12:11:10 +0200

    ClothePiece: Add picture

--- src/Entity/ClothesPiece.php
+++ src/Entity/ClothesPiece.php
@@ -6,10 +6,13 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\ClothesPieceRepository")
+ * @Vich\Uploadable
  */
 class ClothesPiece
 {
@@ -84,6 +87,24 @@ class ClothesPiece
      * @ORM\ManyToMany(targetEntity="App\Entity\Section", inversedBy="clothesPieces")
      */
     private $sections;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="clothespiece_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true))
+     * @var \DateTime
+     */
+    private $updatedAt;
 
 
     public function __construct()
@@ -286,4 +307,52 @@ class ClothesPiece
 
         return $this;
     }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     * @return ClothesPiece
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): ClothesPiece
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+
 }
