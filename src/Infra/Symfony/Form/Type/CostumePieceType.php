bacdb7e3630f2cf56eea31aa8ab1e78336fa3077
Seb Lev <legenyes@gmail.com>
2022-12-18 12:21:26 +0100

    Add autocomplete for clothesCostumePieces collection type

--- src/Infra/Symfony/Form/Type/CostumePieceType.php
+++ src/Infra/Symfony/Form/Type/CostumePieceType.php
@@ -3,6 +3,12 @@
 namespace Infra\Symfony\Form\Type;
 
 use Doctrine\ORM\EntityRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\CrudAutocompleteType;
+use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Infra\EasyAdmin\Controller\ClothesPieceCrudController;
 use Infra\Symfony\Persistance\Doctrine\Entity\ClothesCostumePiece;
 use Infra\Symfony\Persistance\Doctrine\Entity\ClothesPiece;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -12,15 +18,29 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CostumePieceType extends AbstractType
 {
+    private AdminUrlGenerator $urlGenerator;
+    private AdminContextProvider $contextProvider;
+
+    public function __construct(AdminUrlGenerator $urlGenerator, AdminContextProvider $contextProvider)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->contextProvider = $contextProvider;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('piece', EntityType::class, [
+            ->add('piece', CrudAutocompleteType::class, [
                 'class' => ClothesPiece::class,
                 'required' => true,
-                'choice_label' => 'codeDisplayName',
+                'attr' => [
+                    'data-ea-align' => 'left',
+                    'data-ea-widget' => 'ea-autocomplete',
+                    'data-ea-autocomplete-endpoint-url' => $this->getSearchEndpoint(),
+                ],
+                /*'choice_label' => 'codeDisplayName',
                 'query_builder' => fn(EntityRepository $er) => $er->createQueryBuilder('piece')
-                    ->orderBy('piece.code', 'ASC'),
+                    ->orderBy('piece.code', 'ASC'),*/
             ])
             ->add('isDefault')
         ;
@@ -31,5 +51,30 @@ class CostumePieceType extends AbstractType
         $resolver->setDefaults([
             'data_class' => ClothesCostumePiece::class
         ]);
+    }
+
+    protected function getSearchEndPoint(): string
+    {
+        $context = $this->contextProvider->getContext();
+        if (!$context) {
+            throw new \RuntimeException('No context found');
+        }
+        $crud = $context->getCrud();
+        if (!$crud)  {
+            throw new \RuntimeException('No CRUD found');
+        }
+        $page = $crud->getCurrentPage();
+
+        return $this->urlGenerator
+            ->unsetAll()
+            ->set('page', 1)
+            ->setController(ClothesPieceCrudController::class)
+            ->setAction('autocomplete')
+            ->set(AssociationField::PARAM_AUTOCOMPLETE_CONTEXT, [
+                EA::CRUD_CONTROLLER_FQCN => ClothesPieceCrudController::class,
+                'propertyName' => 'name',
+                'originatingPage' => $page,
+            ])
+            ->generateUrl();
     }
 }
